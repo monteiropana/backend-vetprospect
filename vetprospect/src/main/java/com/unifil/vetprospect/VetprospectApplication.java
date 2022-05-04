6bb2c5702b5c244b6f0e6a47c64872f022de545b
ana paula monteiro <anapaulamonteiro978@gmail.com>
2022-05-03 21:27:33 -0300

    configuracao start postgres com docker, veterinario servico e consultar todos veterinarios

--- vetprospect/src/main/java/com/unifil/vetprospect/VetprospectApplication.java
+++ vetprospect/src/main/java/com/unifil/vetprospect/VetprospectApplication.java
@@ -1,7 +1,15 @@
 package com.unifil.vetprospect;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.unifil.vetprospect.models.Veterinario;
+import com.unifil.vetprospect.repository.VeterinarioRepository;
 
 @SpringBootApplication
 public class VetprospectApplication {
@@ -10,4 +18,15 @@ public class VetprospectApplication {
 		SpringApplication.run(VetprospectApplication.class, args);
 	}
 
+	//metodo apenas para exemplo para carregar o banco no inicio do desenvolvimento
+	//@Bean //anotacao que diz que o spring gerencia todo o ciclo de vida
+//	CommandLineRunner initDatabase(VeterinarioRepository veterinarioRepository) { //metodo que sera executado assim que o projeto for inicializado
+//		return args -> {
+//			veterinarioRepository.deleteAll();
+//			List<Veterinario> veterinarios = new ArrayList<Veterinario>();
+//			veterinarios.add(new Veterinario(1l, "Valdir", "Animais de Grande Porte", 12345, "Londrina", "Gleba", "99999999", "Observacoes"));
+//			
+//			veterinarioRepository.saveAll(veterinarios);
+//		};
+//	}
 }
